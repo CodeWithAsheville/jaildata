c3e172a71d7e212a20e95dba048d59f6ce21d5f3
Jay Hill <116148+jayhill@users.noreply.github.com>
2025-09-28 07:49:42 -0400

    #5 add jest config

---
+++ serverless/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/lib', '<rootDir>/api'],
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverageFrom: [
+        'lib/**/*.ts',
+        'api/**/*.ts',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+    ],
+    coverageReporters: ['text', 'lcov', 'html'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+};
