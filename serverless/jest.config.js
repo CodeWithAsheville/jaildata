685413cfe67e5ff45bc57ccdac883ca44e95c552
Jay Hill <116148+jayhill@users.noreply.github.com>
2025-09-29 11:53:10 -0400

    #8 formatting

--- serverless/jest.config.js
+++ serverless/jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/lib', '<rootDir>/api'],
-    testMatch: ['**/__tests__/**/*.test.ts'],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/lib", "<rootDir>/api"],
+    testMatch: ["**/__tests__/**/*.test.ts"],
     transform: {
-        '^.+\\.ts$': 'ts-jest',
+        "^.+\\.ts$": "ts-jest",
     },
     collectCoverageFrom: [
-        'lib/**/*.ts',
-        'api/**/*.ts',
-        '!**/*.d.ts',
-        '!**/node_modules/**',
+        "lib/**/*.ts",
+        "api/**/*.ts",
+        "!**/*.d.ts",
+        "!**/node_modules/**",
     ],
-    coverageReporters: ['text', 'lcov', 'html'],
-    moduleFileExtensions: ['ts', 'js', 'json'],
+    coverageReporters: ["text", "lcov", "html"],
+    moduleFileExtensions: ["ts", "js", "json"],
 };
